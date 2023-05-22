07b64056c8f6a5ede430db87e5b62ae5ce2ff6c2
TheJebForge <jeb@thejebforge.com>
2023-05-22 20:35:06 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+﻿# Really Ban?
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that modifies "Ban from hosted sessions" and "Ban from current session" buttons to require double taps to actually ban
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+2. Place [ReallyBan.dll](https://github.com/TheJebForge/ReallyBan/releases/latest/download/ReallyBan.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
---
+++ ReallyBan/ReallyBan.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine;
+using FrooxEngine.UIX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using BaseX;
+
+namespace ReallyBan
+{
+    public class ReallyBan : NeosMod
+    {
+        public override string Name => "ReallyBan";
+        public override string Author => "TheJebForge";
+        public override string Version => "1.0.0";
+
+        public override void OnEngineInit() {
+            Harmony harmony = new Harmony($"net.{Author}.{Name}");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(FriendsDialog), "OnBanFromAll")]
+        class FriendsDialog_OnBanFromAll
+        {
+            public static bool Prefix(FriendsDialog __instance, ref IButton button, ButtonEventData eventData)
+            {
+                return button.Confirm("", confirmColor: color.Red);
+            }
+        }
+        
+        [HarmonyPatch(typeof(FriendsDialog), "OnBanFromCurrent")]
+        class FriendsDialog_OnBanFromCurrent
+        {
+            public static bool Prefix(FriendsDialog __instance, ref IButton button, ButtonEventData eventData)
+            {
+                return button.Confirm("", confirmColor: color.Red);
+            }
+        }
+    }
+}
